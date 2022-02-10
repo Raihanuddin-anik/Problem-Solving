ea9bc9a1a40487f6a27b216b4ad84037315c67cb
Raihanuddin-anik <67515498+Raihanuddin-anik@users.noreply.github.com>
2022-02-10 23:36:16 +0600

    problem solving

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const CardDistribution = (Name,Distric,Year,birthday,postCode,priority) =>{
+          const data = {Name,Distric,Year}
+        const two = (data.Name).slice(0,2);
+        console.log(two)
+}
+CardDistribution([
+    {Name:'Comilla',Distric:"comila", Year:"2020",birthday:"2020",postCode:"3500",priority:"1"},
+    {Name:'Comilla',Distric:"comila", Year:"2020",birthday:"2020",postCode:"3500",priority:"1"}
+])
